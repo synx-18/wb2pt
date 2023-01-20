b516f6a6a008d7994d5a7a5c5ba012f2f4439886
caro3dc <caro3dc@gmail.com>
2023-01-20 19:58:47 +0800

    darkmode 4/10 divs

--- taco.js
+++ taco.js
@@ -1,20 +1,24 @@
 function darkmode() { // function to run when darkmode button is pressed
 
     
-    var element = document.querySelectorAll(':root, html, nav'); // select root of the document
-    var el2 = document.querySelectorAll("div.cu, div.members"); // select Divs Specifically
+    
+    var element = document.getElementsByTagName('*'); // select root of the document
     
     for (var i = 0; i < element.length; ++i) {
         element[i].classList.toggle("darkmode"); } // toggles class "darkmode" on all elements
 
-    for (var i = 0; i < el2.length; ++i) {
-        el2[i].classList.toggle("darkmode"); // toggles class "darkmode" on all elements
-
-}
-
-var d = document.getElementsByClassName("darkmode"); // code for darkmode transition
+    var d = document.getElementsByClassName("darkmode"); // code for darkmode transition
 
     for( i = 0; i < d.length; i++) {
         d[i].style.transition = "all 0.25s"; // all elements with darkmode class, transition = 0.25s
     }
+
+    var f = document.getElementsByTagName("img");
+
+    for( i = 0; i < d.length; i++) {
+        f[i].style.transition = "1.5s"; // all elements with image class, transition = 1.5s
+    }
+
+
+
 }
