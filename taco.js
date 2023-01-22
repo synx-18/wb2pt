d43d3a8a76283c2536feed42d7ab4ff953f8ceca
caro3dc <caro3dc@gmail.com>
2023-01-23 02:56:16 +0800

    FINAL!!!! [dabs like its 2017]

--- taco.js
+++ taco.js
@@ -13,13 +13,20 @@ function darkmode() { // function to run when darkmode button is pressed
         d[i].style.transition = "all 0.25s"; // all elements with darkmode class, transition = 0.25s
     }
 
+
+    
+
     var f = document.getElementsByTagName("img");
 
     for( i = 0; i < f.length; i++) {
         f[i].style.transition = "1.5s"; // all elements with image class, transition = 1.5s
     }
 
+    var df = document.getElementsByClassName("imi"); // code for darkmode transition
 
+    for( i = 0; i < d.length; i++) {
+        df[i].style.transition = "all 0.25s"; // all elements with darkmode class, transition = 0.25s
+    }
     
 }
 
